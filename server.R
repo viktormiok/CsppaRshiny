6bc2ae2d92e9c4b702507b1faab0a07ff3401b3b
viktormiok <viktormiok@gmail.com>
2021-06-08 07:17:57 +0300

    version 1.0.3

--- server.R
+++ server.R
@@ -4,89 +4,89 @@ server <- function(input, output, session) {
     if(!is.null(input$datafile)){
       
       dat <- read.csv(input$datafile$datapath, sep=";")
-      if(input$var1 == "Gfap") mark = 'gfap_only'
-      if(input$var1 == "Aldh1l1") mark = 'aldh_only' 
-      if(input$var1 == "Colocalization") mark = 'both'
-      marker <- get.data(data = dat,
-                         marker = mark, 
-                         diet = c(0, 5, 15),
-                         mouse = 1:4,
-                         arc = 1:8,
-                         cal_arc = FALSE
+      if(input$var1 == "Gfap") mark='gfap_only'
+      if(input$var1 == "Aldh1l1") mark='aldh_only' 
+      if(input$var1 == "Colocalization") mark='both'
+      marker <- get.data(data=dat,
+                         marker=mark, 
+                         diet=c(0, 5, 15),
+                         mouse=1:4,
+                         arc=1:8,
+                         cal_arc=FALSE
       )
       if(input$var2 == "Chow"){
-        dat_mark2 = marker[[1]]
-        diet2 = 0
+        dat_mark2=marker[[1]]
+        diet2=0
       }
       if(input$var2 == "HFHS 5 days"){
-        dat_mark2 = marker[[2]]
-        diet2 = 5
+        dat_mark2=marker[[2]]
+        diet2=5
       }
       if(input$var2 == "HFHS 15 days"){
-        dat_mark2 = marker[[3]]
-        diet2 = 15
+        dat_mark2=marker[[3]]
+        diet2=15
       }
       output$plot <- renderPlot({
         if(input$analysis == "initial_visualization"){
           
-          if(input$var11 == "Gfap") mark = 'gfap_only'
-          if(input$var11 == "Aldh1l1") mark = 'aldh_only'
-          if(input$var11 == "Colocalization") mark = 'both'
-          marker <- get.data(data = dat,
-                             marker = mark, 
-                             diet = c(0, 5, 15),
-                             mouse = 1:4,
-                             arc = 1:8,
-                             cal_arc = FALSE
+          if(input$var11 == "Gfap") mark='gfap_only'
+          if(input$var11 == "Aldh1l1") mark='aldh_only'
+          if(input$var11 == "Colocalization") mark='both'
+          marker <- get.data(data=dat,
+                             marker=mark, 
+                             diet=c(0, 5, 15),
+                             mouse=1:4,
+                             arc=1:8,
+                             cal_arc=FALSE
           )
           if(input$var21 == "Chow"){
-            dat_mark2 = marker[[1]]
-            diet2 = 0
+            dat_mark2=marker[[1]]
+            diet2=0
           }
           if(input$var21 == "HFHS 5 days"){
-            dat_mark2 = marker[[2]]
-            diet2 = 5
+            dat_mark2=marker[[2]]
+            diet2=5
           }
           if(input$var21 == "HFHS 15 days"){
-            dat_mark2 = marker[[3]]
-            diet2 = 15
+            dat_mark2=marker[[3]]
+            diet2=15
           }
-          p <- gg.aspp(data = dat_mark2,
-                       type_plot = input$plt1,
-                       title = paste(input$var1, ":", input$var2, 'diet')
+          p <- gg.aspp(data=dat_mark2,
+                       type_plot=input$plt1,
+                       title=paste(input$var1, ":", input$var2, 'diet')
           )
           print(p)
         }
         if(input$analysis == "3D_plots"){
-          if(input$var12 == "Gfap") mark = 'gfap_only'
-          if(input$var12 == "Aldh1l1") mark = 'aldh_only'
-          if(input$var12 == "Colocalization") mark = 'both'
-          marker <- get.data(data = dat,
-                             marker = mark, 
-                             diet = c(0, 5, 15),
-                             mouse = 1:4,
-                             arc = 1:8,
-                             cal_arc = FALSE
+          if(input$var12 == "Gfap") mark='gfap_only'
+          if(input$var12 == "Aldh1l1") mark='aldh_only'
+          if(input$var12 == "Colocalization") mark='both'
+          marker <- get.data(data=dat,
+                             marker=mark, 
+                             diet=c(0, 5, 15),
+                             mouse=1:4,
+                             arc=1:8,
+                             cal_arc=FALSE
           )
           if(input$var22 == "Chow"){
-            dat_mark2 = marker[[1]]
-            diet2 = 0
+            dat_mark2=marker[[1]]
+            diet2=0
           }
           if(input$var22 == "HFHS 5 days"){
-            dat_mark2 = marker[[2]]
-            diet2 = 5
+            dat_mark2=marker[[2]]
+            diet2=5
           }
           if(input$var22 == "HFHS 15 days"){
-            dat_mark2 = marker[[3]]
-            diet2 = 15
+            dat_mark2=marker[[3]]
+            diet2=15
           }
           if(input$plt2 == "3D_dens"){
-            d3.aspp(data = dat_mark2,
-                    type_plot = "3D_plot",
-                    window = owin(poly = list(x = c(0, 500, 0), y = c(0, 0, 500))),
-                    title = "Gfap chow diet",
-                    theta = 55,
-                    phi = 20
+            d3.aspp(data=dat_mark2,
+                    type_plot="3D_plot",
+                    window=owin(poly=list(x=c(0, 500, 0), y=c(0, 0, 500))),
+                    title="Gfap chow diet",
+                    theta=55,
+                    phi=20
             )
           } else{
             if(input$plt2 == "3D_scatter"){
@@ -99,8 +99,8 @@ server <- function(input, output, session) {
                               ylab="Y", zlab="Slides", main="gfap", cex=0.3)
               print(ps)
             } else{
-              p <- d3.aspp(data = dat_mark2,
-                           type_plot = "3D_plot_interactive"
+              p <- d3.aspp(data=dat_mark2,
+                           type_plot="3D_plot_interactive"
               )
               print(p)
             }
@@ -108,63 +108,63 @@ server <- function(input, output, session) {
           }
         }
         if(input$analysis == "classification_plot"){
-          marker1 <- get.data(data = dat,
-                              marker = 'gfap_only', 
-                              diet = c(0, 5, 15),
-                              mouse = 1:4,
-                              arc = 1:8,
-                              cal_arc = FALSE
+          marker1 <- get.data(data=dat,
+                              marker='gfap_only', 
+                              diet=c(0, 5, 15),
+                              mouse=1:4,
+                              arc=1:8,
+                              cal_arc=FALSE
           )
-          marker2 <- get.data(data = dat,
-                              marker = 'aldh_only', 
-                              diet = c(0, 5, 15),
-                              mouse = 1:4,
-                              arc = 1:8,
-                              cal_arc = FALSE
+          marker2 <- get.data(data=dat,
+                              marker='aldh_only', 
+                              diet=c(0, 5, 15),
+                              mouse=1:4,
+                              arc=1:8,
+                              cal_arc=FALSE
           )
-          marker3 <- get.data(data = dat,
-                              marker = 'both', 
-                              diet = c(0, 5, 15),
-                              mouse = 1:4,
-                              arc = 1:8,
-                              cal_arc = FALSE
+          marker3 <- get.data(data=dat,
+                              marker='both', 
+                              diet=c(0, 5, 15),
+                              mouse=1:4,
+                              arc=1:8,
+                              cal_arc=FALSE
           )
-          all = rbind(marker1[[1]], marker2[[1]], marker3[[1]]) 
+          all=rbind(marker1[[1]], marker2[[1]], marker3[[1]]) 
           
-          moran = test.saptcorr(dataX = all$X,
-                                dataY = all$Y, 
-                                dataG = all$Gene, 
-                                method = "Moran.I"
+          moran=test.saptcorr(dataX=all$X,
+                                dataY=all$Y, 
+                                dataG=all$Gene, 
+                                method="Moran.I"
           )
           if(input$plt3 == "random_forest"){
-            theme_update(text = element_text(size = 20))
-            pl = randF.class(dataX = all$X,
-                             dataY = all$Y, 
-                             dataG = all$Gene) + 
+            theme_update(text=element_text(size=20))
+            pl=randF.class(dataX=all$X,
+                             dataY=all$Y, 
+                             dataG=all$Gene) + 
               xlab(expression(paste("Distance ", "(", mu, "m)"))) +
               ylab(expression(paste("Distance ", "(", mu, "m)"))) + 
-              scale_fill_manual(values = c("green1", "yellow", "magenta")) +
+              scale_fill_manual(values=c("green1", "yellow", "magenta")) +
               xlim(0,500) +
               ylim(0,450) +
-              geom_text(x = 350, 
-                        y = 350,
-                        label = paste("Moran.I = ", 
+              geom_text(x=350, 
+                        y=350,
+                        label=paste("Moran.I=", 
                                       as.character(round(moran$observed, 3))),
-                        size = 7
+                        size=7
               )
             print(pl)
           } else{
-            theme_update(text = element_text(size = 20))
-            pk <- plot.2d.knn(train_labels = all$Gene, 
-                              train_data = all[,6:7],
-                              k = 5,
-                              X1 = "X",
-                              X2 = "Y") + 
+            theme_update(text=element_text(size=20))
+            pk <- plot.2d.knn(train_labels=all$Gene, 
+                              train_data=all[,6:7],
+                              k=5,
+                              X1="X",
+                              X2="Y") + 
               annotate("text", 
-                       x = 400,
-                       y = 400, 
-                       label = paste("Moran.I = ", as.character(round(t$observed, 3))),
-                       size = 6) + 
+                       x=400,
+                       y=400, 
+                       label=paste("Moran.I=", as.character(round(t$observed, 3))),
+                       size=6) + 
               xlab(expression(paste("Distance ", "(", mu, "m)"))) +
               ylab(expression(paste("Distance ", "(", mu, "m)")))
             print(pk)
@@ -174,70 +174,70 @@ server <- function(input, output, session) {
         if(input$analysis == "density_test_plots"){
           
           if(input$var3 == "none"){
-            dens.aspp(data = dat_mark2, 
-                      #z_lim = z_lim,
-                      type_plot = "test_plot",
-                      window = owin(poly = list(x = c(0, 500, 0), y = c(0, 0, 500))),
-                      nr_sq = input$num_sqr_x,
-                      title = paste(input$var1, ":", input$var2, 'diet'),
-                      col = c("grey", "firebrick2", "yellow"),
-                      ribbon = input$ribbon,
-                      contour = input$contours,
-                      add_square_counts = input$squares,
-                      num_size = 1.2
+            dens.aspp(data=dat_mark2, 
+                      #z_lim=z_lim,
+                      type_plot="test_plot",
+                      window=owin(poly=list(x=c(0, 500, 0), y=c(0, 0, 500))),
+                      nr_sq=input$num_sqr_x,
+                      title=paste(input$var1, ":", input$var2, 'diet'),
+                      col=c("grey", "firebrick2", "yellow"),
+                      ribbon=input$ribbon,
+                      contour=input$contours,
+                      add_square_counts=input$squares,
+                      num_size=1.2
             )
           }else{
             if(input$var3 == "Chow"){
-              dat_mark3 = marker[[1]]
-              diet3 = 0
+              dat_mark3=marker[[1]]
+              diet3=0
             }
             if(input$var3 == "HFHS 5 days"){
-              dat_mark3 = marker[[2]]
-              diet3 = 5
+              dat_mark3=marker[[2]]
+              diet3=5
             }
             if(input$var3 == "HFHS 15 days"){
-              dat_mark3 = marker[[3]]
-              diet3 = 15
+              dat_mark3=marker[[3]]
+              diet3=15
             }
             if(input$plt == "test_plot"){
-              alt <- get.data(data = dat, 
-                              marker = mark, 
-                              diet = diet3, 
-                              mouse = 1:4,
-                              arc = 1:8, 
-                              cal_arc = TRUE
+              alt <- get.data(data=dat, 
+                              marker=mark, 
+                              diet=diet3, 
+                              mouse=1:4,
+                              arc=1:8, 
+                              cal_arc=TRUE
               ) 
-              nul <- get.data(data = dat, 
-                              marker = mark, 
-                              diet = diet2, 
-                              mouse = 1:4,
-                              arc = 1:8, 
-                              cal_arc = TRUE
+              nul <- get.data(data=dat, 
+                              marker=mark, 
+                              diet=diet2, 
+                              mouse=1:4,
+                              arc=1:8, 
+                              cal_arc=TRUE
               )
-              dens.aspp(data = dat_mark3, 
-                        test_null = nul,
-                        test_alt = alt,
-                        #z_lim = z_lim,
-                        type_plot = input$plt,
-                        window = owin(poly = list(x = c(0, 500, 0), y = c(0, 0, 500))),
-                        nr_sq = input$num_sqr_x,
-                        title = paste(input$var1, ":", input$var2, 'vs.', input$var3, 'diet'),
-                        col = c("grey", "firebrick2", "yellow"),
-                        ribbon = input$ribbon,
-                        contour = input$contours,
-                        add_square_counts = input$squares,
-                        num_size = 1.2,
-                        num_size_sig = 1.5
+              dens.aspp(data=dat_mark3, 
+                        test_null=nul,
+                        test_alt=alt,
+                        #z_lim=z_lim,
+                        type_plot=input$plt,
+                        window=owin(poly=list(x=c(0, 500, 0), y=c(0, 0, 500))),
+                        nr_sq=input$num_sqr_x,
+                        title=paste(input$var1, ":", input$var2, 'vs.', input$var3, 'diet'),
+                        col=c("grey", "firebrick2", "yellow"),
+                        ribbon=input$ribbon,
+                        contour=input$contours,
+                        add_square_counts=input$squares,
+                        num_size=1.2,
+                        num_size_sig=1.5
               )
             }else{
-              dens.aspp(data = dat_mark2,
-                        data1 = dat_mark3,
-                        type_plot = input$plt,
-                        window = owin(poly = list(x = c(0, 500, 0), y = c(0, 0, 500))), 
-                        title = paste(input$var1, ":", input$var2, 'vs.', input$var3, 'diet'),
-                        col = c("blue", "grey", "firebrick2"),
-                        ribbon = input$ribbon,
-                        contour = input$contours
+              dens.aspp(data=dat_mark2,
+                        data1=dat_mark3,
+                        type_plot=input$plt,
+                        window=owin(poly=list(x=c(0, 500, 0), y=c(0, 0, 500))), 
+                        title=paste(input$var1, ":", input$var2, 'vs.', input$var3, 'diet'),
+                        col=c("blue", "grey", "firebrick2"),
+                        ribbon=input$ribbon,
+                        contour=input$contours
               )
             }
           }
--- ui.R
+++ ui.R
@@ -20,7 +20,7 @@ inst <- suppressMessages(lapply(c("ggplot2",
                                   "shiny"
 ),
 library,
-character.only = TRUE)
+character.only=TRUE)
 ) 
 # load the code
 setwd("/Users/viktorian.miok/Documents/git/asppa/R")
@@ -46,52 +46,52 @@ ui <- fluidPage(
       
       p("This is an application for testing and visualization of astrocyte spatiall point patterns in n the medial Arcuate Nucleus.\n Your file must be in csv format to upload."),
       
-      fileInput(inputId = "datafile",
-                label = "Upload a data file", 
-                multiple = FALSE,
-                placeholder = "No file selected", 
-                accept = "csv"
+      fileInput(inputId="datafile",
+                label="Upload a data file", 
+                multiple=FALSE,
+                placeholder="No file selected", 
+                accept="csv"
       ),
-      selectInput(inputId = "analysis", label = "Analysis", choices = c("density_test_plots","initial_visualization",
-                                                                        "3D_plots","classification_plot"), width = 200),
+      selectInput(inputId="analysis", label="Analysis", choices=c("density_test_plots","initial_visualization",
+                                                                        "3D_plots","classification_plot"), width=200),
       conditionalPanel(
-        condition = "input.analysis == 'density_test_plots'",
-        selectInput(inputId = "plt", label = "Plot", choices = c("subtraction_square_plot","subtraction_dens_plot",
-                                                                 "test_plot"), width = 200),
-        selectInput(inputId = "var1", label = "Marker", choices = c("Aldh1l1", "Gfap", "Colocalization"), width = 200),
-        selectInput(inputId = "var2", label = "Diet", choices = c("Chow", "HFHS 5 days", "HFHS 15 days"), width = 200),
-        selectInput(inputId = "var3", label = "Compare to", choices = c("Chow", "HFHS 5 days", "HFHS 15 days", "none"), width = 200),
-        checkboxInput(inputId = "contours", label = "Display contours", value = FALSE),
-        checkboxInput(inputId = "squares", label = "Display number of cells per square", value = FALSE),
-        numericInput(inputId = "num_sqr_x", label = "Nr. of squares", value = 10, min = 1, max = 20, width = 100),
-        checkboxInput(inputId = "ribbon", label = "Display a color bar", value = FALSE),
-        actionButton(inputId = "update", label = "Update", width = 130, hight = 5),
+        condition="input.analysis == 'density_test_plots'",
+        selectInput(inputId="plt", label="Plot", choices=c("subtraction_square_plot","subtraction_dens_plot",
+                                                                 "test_plot"), width=200),
+        selectInput(inputId="var1", label="Marker", choices=c("Aldh1l1", "Gfap", "Colocalization"), width=200),
+        selectInput(inputId="var2", label="Diet", choices=c("Chow", "HFHS 5 days", "HFHS 15 days"), width=200),
+        selectInput(inputId="var3", label="Compare to", choices=c("Chow", "HFHS 5 days", "HFHS 15 days", "none"), width=200),
+        checkboxInput(inputId="contours", label="Display contours", value=FALSE),
+        checkboxInput(inputId="squares", label="Display number of cells per square", value=FALSE),
+        numericInput(inputId="num_sqr_x", label="Nr. of squares", value=10, min=1, max=20, width=100),
+        checkboxInput(inputId="ribbon", label="Display a color bar", value=FALSE),
+        actionButton(inputId="update", label="Update", width=130, hight=5),
       ),      
       conditionalPanel(
-        condition = "input.analysis == 'initial_visualization'",
-        selectInput(inputId = "plt1", label = "Plot", choices = c("hexagonal_heatmap","scatter_plot","polygon",
-                                                                  "density_contour"), width = 200),
-        selectInput(inputId = "var11", label = "Marker", choices = c("Aldh1l1", "Gfap", "Colocalization"), width = 200),
-        selectInput(inputId = "var21", label = "Diet", choices = c("Chow", "HFHS 5 days", "HFHS 15 days"), width = 200),
-        actionButton(inputId = "update", label = "Update", width = 130, hight = 5)
+        condition="input.analysis == 'initial_visualization'",
+        selectInput(inputId="plt1", label="Plot", choices=c("hexagonal_heatmap","scatter_plot","polygon",
+                                                                  "density_contour"), width=200),
+        selectInput(inputId="var11", label="Marker", choices=c("Aldh1l1", "Gfap", "Colocalization"), width=200),
+        selectInput(inputId="var21", label="Diet", choices=c("Chow", "HFHS 5 days", "HFHS 15 days"), width=200),
+        actionButton(inputId="update", label="Update", width=130, hight=5)
       ),
       conditionalPanel(
-        condition = "input.analysis == '3D_plots'",
-        selectInput(inputId = "plt2", label = "Plot", choices = c("3D_dens","3D_scatter","3D_dens_interactive"), width = 200),
-        selectInput(inputId = "var12", label = "Marker", choices = c("Aldh1l1", "Gfap", "Colocalization"), width = 200),
-        selectInput(inputId = "var22", label = "Diet", choices = c("Chow", "HFHS 5 days", "HFHS 15 days"), width = 200),
-        actionButton(inputId = "update", label = "Update", width = 130, hight = 5)
+        condition="input.analysis == '3D_plots'",
+        selectInput(inputId="plt2", label="Plot", choices=c("3D_dens","3D_scatter","3D_dens_interactive"), width=200),
+        selectInput(inputId="var12", label="Marker", choices=c("Aldh1l1", "Gfap", "Colocalization"), width=200),
+        selectInput(inputId="var22", label="Diet", choices=c("Chow", "HFHS 5 days", "HFHS 15 days"), width=200),
+        actionButton(inputId="update", label="Update", width=130, hight=5)
       ),
       conditionalPanel(
-        condition = "input.analysis == 'classification_plot'",
-        selectInput(inputId = "plt3", label = "Plot", choices = c("random_forest","knn_network"), width = 200),
-        selectInput(inputId = "var23", label = "Diet", choices = c("Chow", "HFHS 5 days", "HFHS 15 days"), width = 200),
-        actionButton(inputId = "update", label = "Update", width = 130, hight = 5)
+        condition="input.analysis == 'classification_plot'",
+        selectInput(inputId="plt3", label="Plot", choices=c("random_forest","knn_network"), width=200),
+        selectInput(inputId="var23", label="Diet", choices=c("Chow", "HFHS 5 days", "HFHS 15 days"), width=200),
+        actionButton(inputId="update", label="Update", width=130, hight=5)
       )
     ),
     mainPanel(
       h3("Result"),
-      plotOutput(outputId = "plot", height = "700px", width = "700px")
+      plotOutput(outputId="plot", height="700px", width="700px")
     )
   )
 )
